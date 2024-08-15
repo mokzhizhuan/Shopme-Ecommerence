6c992dde7be0ffa684f686b824bcf60c8cfd9431
MokZhizhuan(NeoWolf) <gamerdark44@outlook.com>
2024-08-15 09:42:05 +0800

    customer fixes

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerNotFoundException.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerNotFoundException.java
@@ -2,8 +2,8 @@ package com.shopme.customer;
 
 public class CustomerNotFoundException extends Exception {
 
-	public CustomerNotFoundException(String string) {
-		// TODO Auto-generated constructor stub
+	public CustomerNotFoundException(String message) {
+		super(message);
 	}
 
 }
